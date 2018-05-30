736db61e67d6ef86c24ac00f8ec31d3e2e1f8252
Megan Arellano <ssciolist@gmail.com>
2018-05-30 09:42:57 -0600

    fixes search form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,16 @@ class WelcomeController < ApplicationController
   attr_reader :headers
 
   def index
-    conn = Faraday.new(:url => 'https://od-api.oxforddictionaries.com/api/v1/inflections/en/foxes')
+
+    if params[:text]
+      search
+    else
+    end
+
+  end
+
+  def search
+    conn = Faraday.new(:url => "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{params[:text]}")
     with_auth = conn.get do |req|
       req.headers["Accept"] = "application/json"
       req.headers["app_id"] = ENV['OXFORD_APP_ID']
@@ -12,9 +21,6 @@ class WelcomeController < ApplicationController
     raw_result = JSON.parse(with_auth.body, symbolize_names: true)
     root_word = raw_result[:results][0][:lexicalEntries][1][:inflectionOf][0][:text]
     valid_word =  raw_result[:results][0][:word]
-
-    require 'pry'; binding.pry
-
   end
 
   def headers
