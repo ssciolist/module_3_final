8c8a18f24f51d1c052a298d8b62e74dc0c13a1c8
Megan Arellano <ssciolist@gmail.com>
2018-05-30 11:58:16 -0600

    failed to implement vcr in the final 10 minutes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,16 +2,17 @@ class WelcomeController < ApplicationController
   attr_reader :headers
 
   def index
-
     if params[:text]
       @word = params[:text]
       search = WordSearchService.new(params[:text])
+
       if search.request_status == 200
         @root_word = search.root_word
         render :valid
       else
         render :invalid
       end
+      
     end
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,14 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
+
+# require 'webmock/rspec'
+# require 'vcr'
+#
+# VCR.configure do |config|
+#   config.cassette_library_dir = "spec/cassettes"
+#   config.hook_into :webmock
+# end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
