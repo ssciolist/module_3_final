3ece21e88d8ec4b9854403e16052e5d05450be57
Megan Arellano <ssciolist@gmail.com>
2018-05-30 09:28:59 -0600

    can find root word and valid word in hard coded search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,18 @@ class WelcomeController < ApplicationController
   attr_reader :headers
 
   def index
-    # conn = Faraday.new(:url => 'https://od-api.oxforddictionaries.com/api/v1/inflections/en/swimming')
-    # conn.get do |req|
-    #   req.headers["Accept"] = "application/json"
-    #   req.headers["app_id"] = ENV['OXFORD_APP_ID']
-    #   req.headers["app_key"] = ENV['OXFORD_APP_KEY']
-    # end
-    #
-    # require 'pry'; binding.pry
+    conn = Faraday.new(:url => 'https://od-api.oxforddictionaries.com/api/v1/inflections/en/foxes')
+    with_auth = conn.get do |req|
+      req.headers["Accept"] = "application/json"
+      req.headers["app_id"] = ENV['OXFORD_APP_ID']
+      req.headers["app_key"] = ENV['OXFORD_APP_KEY']
+    end
+
+    raw_result = JSON.parse(with_auth.body, symbolize_names: true)
+    root_word = raw_result[:results][0][:lexicalEntries][1][:inflectionOf][0][:text]
+    valid_word =  raw_result[:results][0][:word]
+
+    require 'pry'; binding.pry
 
   end
 
