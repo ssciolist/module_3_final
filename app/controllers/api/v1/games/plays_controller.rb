61ba219f97dd7524df0476c7ec146a3c4647da38
Megan Arellano <ssciolist@gmail.com>
2018-06-02 10:34:06 -0600

    adds private check for valid word in games controller for more SRP"

--- app/controllers/api/v1/games/plays_controller.rb
+++ app/controllers/api/v1/games/plays_controller.rb
@@ -1,12 +1,20 @@
 class Api::V1::Games::PlaysController < ApplicationController
+  before_action :check_for_valid_word
   def create
-    word = Word.new(params[:word])
     play = Play.new(user_id: params[:user_id], game_id: params[:id], word: params[:word])
-      if word.valid?
-        play.save
+      if play.save
         render json: {message: 'success'}, status: 201
       else
-        render json: {message: "#{play.word} is not a valid word"}
+        render json: {message: "Hm, did you send us all the right info? Something went wrong."}
       end
+  end
+
+  private
+
+  def check_for_valid_word
+    word = Word.new(params[:word])
+    unless word.valid?
+      render json: {message: "#{word.input} is not a valid word"}
+    end
   end
 end
