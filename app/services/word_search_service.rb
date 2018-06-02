3bd11361c6791096af730a55e47b0c4e4767dc68
Megan Arellano <ssciolist@gmail.com>
2018-06-01 19:53:45 -0600

    refactor for 'Service object is built in a way that allows portions of it to be reusable. An un-refactored example would be using string interpolation to build the API request.'

--- app/services/word_search_service.rb
+++ app/services/word_search_service.rb
@@ -3,18 +3,16 @@ class WordSearchService
 
   def initialize(word)
     @word = word
-  end
-
-  def conn
-    Faraday.new(:url => "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{@word}")
+    @conn = Faraday.new(:url => "https://od-api.oxforddictionaries.com") do |f|
+      f.adapter Faraday.default_adapter
+      f.headers["Accept"] = "application/json"
+      f.headers["app_id"] = ENV['OXFORD_APP_ID']
+      f.headers["app_key"] = ENV['OXFORD_APP_KEY']
+    end
   end
 
   def word_request
-    conn.get do |req|
-      req.headers["Accept"] = "application/json"
-      req.headers["app_id"] = ENV['OXFORD_APP_ID']
-      req.headers["app_key"] = ENV['OXFORD_APP_KEY']
-    end
+    @conn.get("/api/v1/inflections/en/#{@word}")
   end
 
   def request_status
